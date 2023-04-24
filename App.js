b7a113201fb6a4f3e45a0521d2f8447749741adc
Guilherme Machado <jmachado.gui@gmail.com>
2023-04-23 22:48:53 -0300

    feat: add background image blurred transition animatiojn

--- App.js
+++ App.js
@@ -18,11 +18,41 @@ const imageW = width * 0.7;
 const imageH = imageW * 1.54;
 
 export default () => {
+    const scrollX = React.useRef(new Animated.Value(0)).current;
+    
     return (
         <View style={{ flex: 1, backgroundColor: '#000' }}>
             <StatusBar hidden /> 
-            <FlatList 
+            <View style={StyleSheet.absoluteFillObject}>
+                {data.map((image, index) => {
+                    const inputRange = [
+                        (index - 1) * width,
+                        index * width,
+                        (index + 1) *  width
+                    ]
+                    const opacity = scrollX.interpolate({
+                        inputRange,
+                        outputRange: [0, 1, 0]
+                    })
+                    return <Animated.Image 
+                        key={`image-${index}`}
+                        source={{uri: image}}
+                        style={[
+                            StyleSheet.absoluteFillObject,
+                            {
+                                opacity: opacity
+                            } 
+                        ]}
+                        blurRadius={40 }
+                    />
+                })}
+            </View>
+            <Animated.FlatList 
                 data={data}
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {x:scrollX}}}],
+                    {useNativeDriver: true}
+                )}
                 horizontal
                 pagingEnabled
                 keyExtractor={(_, index) => index.toString()}
