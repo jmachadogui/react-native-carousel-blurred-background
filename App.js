c2723282603ed74ef489a13703cd29ce58d185a3
Guilherme Machado <jmachado.gui@gmail.com>
2023-04-23 22:31:55 -0300

    feat: add images listed horizontally

--- App.js
+++ App.js
@@ -21,6 +21,21 @@ export default () => {
     return (
         <View style={{ flex: 1, backgroundColor: '#000' }}>
             <StatusBar hidden />
+            <FlatList 
+                data={data}
+                horizontal
+                pagingEnabled
+                keyExtractor={(_, index) => index.toString()}
+                renderItem={({item}) => {
+                    return <View>
+                         <Image source={{uri: item.toString()}} style={{
+                            width: imageW,
+                            height: imageH,
+                            resizeMode:'cover'
+                         }}/>
+                    </View>
+                }}    
+            />
         </View>
     );
 };
